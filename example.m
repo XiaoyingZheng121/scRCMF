792390e2a7db0104dd78d1d3fb2c89ae71ae6dcc
XiaoyingZheng121 <34908134+XiaoyingZheng121@users.noreply.github.com>
2018-12-01 21:02:25 +0800

    Add files via upload

---
+++ example.m
@@ -0,0 +1,52 @@
+%% Setup paths and load data
+clear;
+clc;
+echo on;
+addpath('Data');
+addpath('Cluster');
+addpath('TC');
+
+
+cell_data=load('sim_data.mat');
+
+%group2=idx;
+data = cell_data.X1;
+%data=X;
+% Load all gene names as string
+%load gene_name.mat;
+
+ %% Optional step: preprocess data by selecting a subset of genes
+%data(all(var(data,0,2)<0.5,2),:)=[];
+% 
+%% Step 1: Run scRCMF to identify  low rank structure 
+K=gap_cluster(data');    % K is the number of clusters: can be specified by user, or
+            % if not given (K = []), it will be inferred
+
+[W,H] = scRCMF_c(data,K);
+
+% Output
+%   --  kopt: Number of clusters
+%   --  W: left matrix
+%   --  H: right matrix
+
+%% Step 2: Run scRCMF to identify subpopulations and transition states 
+c=0.6;       % c is the thresold: can be specified by user
+Co_module  = Cell_cluster(H,W,c);%
+tc_module = identify_tc(H,c);
+ % Output
+%   -- Co_module: Cell clusters corresponding key genes.
+%  -- tc_module£ºtransition states between  cell clusters
+
+%% Step 3: Run scRCMF to analyse transition states 
+TE=scRCMF_TE(H);
+ % Output
+%  -- TE: Entropy of transition states corresponding cluster label
+
+U=scRCMF_TP(data,H,Co_module,tc_module);
+%  -- pt: transition probability from  transition state to corresponding cell cluster
+
+%% Step 3: select differnential gene 
+%gene_set=gene_pattern(cell_data,Co_module,tc_module,K,gene_name);%HEE data
+
+
+
